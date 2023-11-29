725f83f7a27eb869875f53323abf2d07f27fa434
defnot001 <marc.miller994@gmail.com>
2023-11-29 11:51:09 +0100

    added custom tooltips #10

--- src/main/java/io/github/defnot001/defmod/block/custom/SoundBlock.java
+++ src/main/java/io/github/defnot001/defmod/block/custom/SoundBlock.java
@@ -1,16 +1,23 @@
 package io.github.defnot001.defmod.block.custom;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class SoundBlock extends Block {
     public SoundBlock(Properties properties) {
@@ -24,5 +31,11 @@ public class SoundBlock extends Block {
         level.playSound(player, pos, SoundEvents.NOTE_BLOCK_XYLOPHONE.value(), SoundSource.BLOCKS, 1f, 1f);
 
         return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable BlockGetter level, List<Component> tooltip, TooltipFlag flag) {
+        tooltip.add(Component.translatable("tooltip.defmod.sound_block.tooltip"));
+        super.appendHoverText(stack, level, tooltip, flag);
     }
 }
--- src/main/java/io/github/defnot001/defmod/item/custom/MetalDetectorItem.java
+++ src/main/java/io/github/defnot001/defmod/item/custom/MetalDetectorItem.java
@@ -5,11 +5,17 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class MetalDetectorItem extends Item {
     public MetalDetectorItem(Properties properties) {
@@ -59,5 +65,11 @@ public class MetalDetectorItem extends Item {
 
     private boolean isValuableBlock(BlockState state) {
         return state.is(Blocks.IRON_ORE) || state.is(Blocks.RAW_IRON_BLOCK);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltipComponents, TooltipFlag isAdvanced) {
+        tooltipComponents.add(Component.translatable("tooltip.defmod.metal_detector.tooltip"));
+        super.appendHoverText(stack, level, tooltipComponents, isAdvanced);
     }
 }
